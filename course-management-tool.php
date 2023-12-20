b749da52eb755a1dc4ba0a8c4fbcb71456d65664
nikoroytio <roytioniko@gmail.com>
2023-12-20 13:31:41 +0200

    made main file to register plugin and call other files

--- course-management-tool.php
+++ course-management-tool.php
@@ -1,10 +1,36 @@
 <?php
 /*
 Plugin Name: Course Management Tool
-Description: management tool for addinc courses
+Description: management tool for adding courses
+Text Domain: course-management-tool
 Version: 1.0
 Author: Niko Röytiö
 Author URI: https://nikoroytio.com
 */
+
+// Include other PHP files
+include_once plugin_dir_path(__FILE__) . 'includes/custom-post-types.php';
+include_once plugin_dir_path(__FILE__) . 'includes/taxonomies.php';
+include_once plugin_dir_path(__FILE__) . 'includes/meta-boxes.php';
+
+// Activation and deactivation hooks
+//for naming we are using cm for course management
+//we are also flushing, because these changes affects permalinks
+function cm_activate() {
+    cm_register_course_post_type();
+    cm_register_course_taxonomy();
+    flush_rewrite_rules();
+}
+
+//activation hook that the plugin functions are called when activated
+register_activation_hook(__FILE__, 'cm_activate');
+
+//deactivation hook that the plugin can be deactivated
+function cm_deactivate() {
+    flush_rewrite_rules();
+}
+register_deactivation_hook(__FILE__, 'cm_deactivate');
+
+
 ?>
 
